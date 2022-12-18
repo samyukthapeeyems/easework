7f622a847b31986c1f75fd77440801d82f39ecb3
samyukthapeeyems <samyukthapeeyems@gmail.com>
2022-12-18 11:12:05 +0530

    minor changes

--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -1,45 +1,96 @@
-import {TextInput, View, StyleSheet, Text} from 'react-native';
-import {Formik} from 'formik';
+import {TextInput, View, StyleSheet, Text, Modal, FlatList} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import Button from '../components/Button';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({item, onPress, backgroundColor, textColor}) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Text style={[styles.title, textColor]}>{item.title}</Text>
+  </TouchableOpacity>
+);
 
 export default function AddTodo() {
   const [todoName, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [modalVisible, setModalVisible] = useState(true);
+
   async function createTodo() {
-    try {
-      await firestore().collection('todos').add({
-        todoName: todoName,
-        createdAt: new Date(),
-        completed: false,
-      });
-    } catch (e) {
-      console.log(e);
-    }
+    await firestore().collection('todos').add({
+      todoName: todoName,
+      createdAt: new Date(),
+      completed: false,
+    });
   }
 
-  return (
-    // <Formik
-    //   initialValues={{taskName: ''}}
-    //   onSubmit={values =>createTodo(values)}>
-    //   {({handleChange, handleBlur, handleSubmit, values}) => (
-    <View style={{flex: 1, margin: 10}}>
-      <Text style={{color: 'black'}}>Set A Goal, jot it here 🧘</Text>
-      <TextInput
-        onChangeText={setTodo}
-        // onBlur={handleBlur('taskName')}
-        value={todoName}
-      />
+  async function markCompleted(_id) {
+    await firestore().collection('todos').doc(_id).update({
+      completed: true,
+    });
+  }
 
-      <Button
-        style={styles.button}
-        textStyle={styles.buttonText}
-        onPress={createTodo}>
-        Add Todo
-      </Button>
+  useEffect(() => {
+    firestore()
+      .collection('todos')
+      .get()
+      .then(snapshots => {
+        let todos = [];
+        snapshots.forEach(snapshot => {
+          todos.push({
+            id: snapshot.id,
+            ...snapshot.data(),
+          });
+        });
+        setTodos(todos);
+      });
+  }, []);
+
+  return (
+    <View style={{flex: 1}}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}>
+        <View style={{flex: 1, margin: 10}}>
+          <Text style={{color: 'black'}}>Set A Goal, jot it here 🧘</Text>
+          <TextInput onChangeText={setTodo} value={todoName} />
+          <Button
+            style={styles.button}
+            textStyle={styles.buttonText}
+            onPress={async () => {
+              await createTodo();
+              setModalVisible(!modalVisible);
+            }}>
+            Add Todo
+          </Button>
+        </View>
+      </Modal>
+
+
+<View>
+    <Text>TODO</Text>
+    <Button></Button>
+</View>
+      {/* <FlatList
+       data={todos}
+       key={todos}
+      /> */}
     </View>
-    //   )}
-    // </Formik>
   );
 }
 
